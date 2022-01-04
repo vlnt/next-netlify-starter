03baaee13b2a68925193268a5d15b5a05712aabb
Valentyn Mirosh <vlnt@ua.fm>
2022-01-05 00:24:02 +0200

    Initial commit

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return <header>
+    <h1 className="title">{title}</h1><br/>
+    <h2>Let's learn <span id='labelNext'><a href='https://nextjs.org'>Next.js</a></span></h2>
+    <h4>Read {' '} 
+        <Link href="/posts/first-post">
+                <a>this page!</a>
+        </Link>
+      </h4>
+  </header>
 }
