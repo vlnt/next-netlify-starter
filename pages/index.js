15e08bcd6474752d7ad2fa112999aacf6ab73213
Valentyn Mirosh <vlnt@ua.fm>
2022-01-13 09:35:06 +0200

    test commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my blog!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
